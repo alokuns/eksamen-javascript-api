73e73aa9a464b0ef98f8d81ae0fec1eb0ad807a4
alokuns <alon.okun@gmail.com>
2024-05-06 12:47:43 +0200

    Lagt til funksjoner for henting av header

--- JS/helpers.js
+++ JS/helpers.js
@@ -2,3 +2,17 @@
 const USERBASE_URL = "https://crudapi.co.uk/api/v1/user";
 const COUNTRIES_URL = "https://restcountries.com/v3.1/all";
 const API_KEY = "bmXA_-p_tY6_h_SxX93ivVoX8BGzS6RzZ8h8wqWqvS6nY6n0rg";
+
+// Get headers
+const getHeadersWithKey = () => {
+  return {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  };
+};
+
+const getHeaders = () => {
+  return {
+    "Content-Type": "application/json",
+  };
+};
