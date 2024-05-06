6aa02d4d4e48fc5b55d393614fe770362c4acfa1
alokuns <alon.okun@gmail.com>
2024-05-06 12:41:01 +0200

    Fetching av urls og api-key

---
+++ JS/helpers.js
@@ -0,0 +1,4 @@
+// API Fetch
+const USERBASE_URL = "https://crudapi.co.uk/api/v1/user";
+const COUNTRIES_URL = "https://restcountries.com/v3.1/all";
+const API_KEY = "bmXA_-p_tY6_h_SxX93ivVoX8BGzS6RzZ8h8wqWqvS6nY6n0rg";
